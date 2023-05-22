31f89f44828584613f3b5b34374fe8e2811488e0
Zachary Mcleod <zacharymcleod@zacharys-air.home>
2023-05-22 08:15:25 -0700

    Condensed everything into functions. Also, restarts if the user isn't satisfied.

--- day_trip_generator_functions.py
+++ day_trip_generator_functions.py
@@ -1,6 +1,8 @@
 import random
 import time
 
+
+# Variables
 destinations = ['The Grand Canyon', 'Washington DC', 'Seattle, Washington', 'New York City']
 restaurants = ['Steak-House', 'Chinese restaurant', 'Mexican restaurant', 'Mongolian Barbecue', 'Food Truck']
 transportations = ['Helicopter', 'Car', 'Plane', 'Train']
@@ -14,16 +16,16 @@ random_entertainment = random.choice(entertainments)
 day_trip_lists = [random_destination, random_restaurant, random_transportation, random_entertainment]
 day_trip_lists_str = ['destination', 'restaurant', 'transportation', 'entertainment']
 
-def prompt_random_item_choice():
+
+# Functions
+def prompt_every_variable_choice():
     choice = ''
-    while choice != 'yes' and choice != 'no':
+    while True:
         choice = input(f'Would you like a random day trip for every variable?: (destinations, restaurants, transportations, entertainments): ').lower()
         if choice == 'yes':
             return True
-            break
         elif choice == 'no':
             return False
-            break
         else:
             print('''I'm sorry, I didn't get that...
                   ''')
@@ -44,7 +46,6 @@ def what_events_to_randomize(all_items):
             {non_chosen_items}?: ''').lower()
         else:
             return non_chosen_items
-            break
         if choice in non_chosen_items:
             chosen_items.append(choice)
             non_chosen_items.remove(choice)
@@ -55,28 +56,56 @@ def what_events_to_randomize(all_items):
             print('Sorry, what?')
             continue
 
+def satisfied():
+    satisfied = input("are you satisfied with the results? Type 'yes for completion or 'no' to regenerate Day Trip: ")
+    if satisfied.lower() == 'yes':
+        time.sleep(1)
+        print('Complete')
+        return True
+    elif satisfied.lower() == 'no':
+        time.sleep(1)
+        print("Let's restart...")
+        time.sleep(2)
+        return False
 
-
-if prompt_random_item_choice():
-    print(f'''Glad to hear it!
-    Your Day Trip events include these in them:
-
-    {random_destination}
-    A {random_transportation}
-    {random_entertainment}
-    and a {random_restaurant}
-    ''')
-else:
-    events_to_randomize = what_events_to_randomize(day_trip_lists_str)
+def print_day_trip_events():
     print(f'''Good to know!
-Your Day Trip events include these in them:''')
+    Your Day Trip events include these in them:''')
+    time.sleep(2)
     if 'destination' in events_to_randomize:
         print(random_destination)
+        time.sleep(1)
     if 'transportation' in events_to_randomize:
-        print(random_transportation)
+        print(f'Riding {random_transportation}')
+        time.sleep(1)
     if 'entertainment' in events_to_randomize:
         print(random_entertainment)
+        time.sleep(1)
     if 'restaurant' in events_to_randomize:
         print(random_restaurant)
+        time.sleep(1)
 
-    
+def welcome_to_day_trip_generator():
+    print('''Welcome to Day Trip Generator!''')
+    time.sleep(2)
+
+
+# Day Trip Generator
+welcome_to_day_trip_generator()
+while True:
+    if prompt_every_variable_choice():
+        events_to_randomize = day_trip_lists_str
+        print_day_trip_events()
+        
+    else:
+        events_to_randomize = what_events_to_randomize(day_trip_lists_str)
+        print_day_trip_events()
+
+    if satisfied():
+        break
+    else:
+        continue
+
+
+
+
